ad482e840d6a2b8a97ae6a0563e8f1e88752896e
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-05-08 21:08:55 +0530

    set post request

--- server/index.js
+++ server/index.js
@@ -14,6 +14,23 @@ const db  =  mysql.createConnection({
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended: true}));
+ 
+app.post("/api/insert",(req, res)=>{
+
+    const firstName = req.body.firstName;
+    const lastName  = req.body.lastName;
+    const salary  = req.body.salary;
+    const address  = req.body.address;
+
+    const sqlInsert = "INSERT INTO customer (firstName,lastName,salary,address)VALUES(?,?,?,?);"
+    db.query(sqlInsert, [firstName,lastName,salary,address],(err,result)=>{
+        console.log(result);
+        // res.send("hell world2");
+       
+    })
+});
+
+
 
 app.listen(3001,() => {
     console.log("runing on port 3001");
